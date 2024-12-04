0d22b63c8416b3e402a823328905e79236fefb89
jiri.strouhal <jiri.strouhal@gmail.com>
2024-12-04 18:28:18 +0100

    Sketch out the validator in few comments

---
+++ tool/validator/validator.py
@@ -0,0 +1,10 @@
+# look at the solution and tests
+
+# for each test:
+# 1. assess if the test can be currently run
+# 2. choose the test implementation accordingly to the solution format
+# 3. write the test implementation
+# 4. run the test
+# 5. provide a critique for the result
+
+# store the solution with updated test critiques
