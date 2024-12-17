382b1ed972bbc4d8326924221d4d8e81cf7c2061
jiri.strouhal <jiri.strouhal@gmail.com>
2024-12-17 07:15:14 +0100

    Extend prompts, rename variable

--- tool/validator/text.py
+++ tool/validator/text.py
@@ -17,7 +17,7 @@ class SolutionWithTestsToRun(pydantic.BaseModel):
     task: str
     context: str
     requirements: list[str]
-    sources: dict[str, str]
+    resources: dict[str, str]
     solution_structure: list[str]
     tests_to_run: dict[int, _Test]
     run_tests: dict[int, _Test]
@@ -34,7 +34,7 @@ class SolutionWithTestsToRun(pydantic.BaseModel):
         return SolutionWithTestsToRun(
             task=solution.task,
             context=solution.context,
-            sources=solution.resources,
+            resources=solution.resources,
             requirements=solution.requirements,
             solution_structure=solution.solution_structure,
             tests_to_run=tests_to_run,
@@ -51,7 +51,7 @@ class SolutionWithTestsToRun(pydantic.BaseModel):
             task=solution_with_next_test.task,
             requirements=solution_with_next_test.requirements,
             context=solution_with_next_test.context,
-            resources=solution_with_next_test.sources,
+            resources=solution_with_next_test.resources,
             solution_structure=solution_with_next_test.solution_structure,
             tests=tests,
             solution=solution_with_next_test.solution,
@@ -88,11 +88,15 @@ You are a helpful assistant, that helps to verify the correctness of the solutio
 
 Here is a solution to some task:
 {solution}
+Here are resources used to solve the task:
+{resources}
 
 You need to provide answers to these questions related to the solution:
 {questions}
 
-Write only the answers. Do not write anything else.
+When answering the questions, follow these guidelines:
+1) Respond in whole sentences. If the question is something like "Is it true, that XY?", answer with "Yes, it is true that XY." or "No, it is not true that XY."
+2) Write only the answers. Do not write anything else.
 """
 
 
@@ -108,11 +112,9 @@ Please, provide me with a critique of the solution. The critique should contain
 - place in the solution, where the error is, if applicable
 - a single-sentence suggestion, how to fix the error, if applicable.
 
-Test passes if the answer to a question is positive.
-
-If all the answers received positive responses, write also "TEST_PASSED".
-
-Do not write anything else.
+When providing critique, follow these guidelines:
+1) Focus on match between the test description and the test result.
+2) If all the answers match the test passing, write also "TEST_PASSED", otherwise write "TEST_FAILED".
 """
 
 
@@ -122,7 +124,7 @@ TEXT_TESTER_END = "__text_tester_end__"
 _model = ChatOpenAI(name="gpt-4o-mini")
 
 
-def prepareSolution_with_tests_to_run(solution: _Solution) -> SolutionWithTestsToRun:
+def prepare_solution_with_tests_to_run(solution: _Solution) -> SolutionWithTestsToRun:
     return SolutionWithTestsToRun.fromSolution(solution)
 
 
@@ -138,7 +140,7 @@ def any_next_test(
     return "end"
 
 
-def returnSolution_with_updated_tests(
+def return_solution_with_updated_tests(
     solution_with_next_test: SolutionWithTestsToRun,
 ) -> _Solution:
     return SolutionWithTestsToRun.toSolution(solution_with_next_test)
@@ -167,7 +169,9 @@ def run_test(solution_with_next_test: SolutionWithTestsToRun) -> SolutionWithTes
     test_id = list(solution_with_next_test.tests_to_run.keys())[0]
     test = solution_with_next_test.tests_to_run.pop(test_id)
     prompt = TEST_RUNNER_PROMPT.format(
-        solution=solution_with_next_test.solution, questions=test.implementation
+        solution=solution_with_next_test.solution,
+        questions=test.implementation,
+        resources=solution_with_next_test.resources,
     )
     response = _model.invoke([SystemMessage(content=prompt)])
     test.last_output = str(response.content)
@@ -195,13 +199,13 @@ def criticize(solution: _Solution) -> _Solution:
 def get_text_validator_builder() -> StateGraph:
     text_validator_builder = StateGraph(_Solution)
     text_validator_builder.add_node(
-        "prepareSolution_with_no_test_to_run_next", prepareSolution_with_tests_to_run
+        "prepareSolution_with_no_test_to_run_next", prepare_solution_with_tests_to_run
     )
     text_validator_builder.add_node("pick_test", pick_test)
     text_validator_builder.add_node("implement_next_test", implement_test)
     text_validator_builder.add_node("run_test", run_test)
     text_validator_builder.add_node(
-        "returnSolution_with_updated_tests", returnSolution_with_updated_tests
+        "returnSolution_with_updated_tests", return_solution_with_updated_tests
     )
     text_validator_builder.add_node("critic", criticize)
 
