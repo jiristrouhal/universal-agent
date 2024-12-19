647cc0da79e93cc4b3a0e73c7a4836ccc96f5a9d
jiri.strouhal <jiri.strouhal@gmail.com>
2024-12-19 09:43:00 +0100

    Update ResourceManager to avoid collecting trivial knowledge and repeating requests

--- tool/proposer/resources.py
+++ tool/proposer/resources.py
@@ -6,39 +6,47 @@ from langgraph.prebuilt.chat_agent_executor import create_react_agent
 from langchain_community.tools.wikipedia.tool import WikipediaQueryRun, WikipediaAPIWrapper
 from langchain_community.tools import DuckDuckGoSearchRun
 
+from tool.logs import get_logger
 from tool.models import Solution, Solution, ResourceForm, EMPTY_RESOURCE, Resource as _Resource
 from tool.memory.resource_db import ResourceDB as _ResourceDB, new_custom_database as _resource_db
 
 
+logger = get_logger()
+
+
 _IDENTIFY_SOURCES_PROMPT = """
-You are a helpful assistant, that helps me to design solution to a task.
+You are a helpful assistant, that collects for me a knowledge necessary for solving given task.
+The task can be either to write a code, do a research or answer a question. T
+The task can be also very trivial, in which case I want you to omit the collection of resources.
 
 I will give you the following information:
 
 Task: ...
 Context: ...
-Solution draft: ...
+Solution structure: ...
 Existing resources: ...
 
-I need you to give list me resources, that
-a) are required to design the solution and
-b) are not provided in the 'Existing resources'.
-c) are not based common knowledge or general facts.
+You will write for me a list of new requests for resources, that are not among the Existing resources.
+It is possible that all requests are already written in the Existing resources.
+In such case, you dont need to write anything.
 
-Respond in a form of list. Omit the resources that are already in the 'Existing resources'.
-For each item, provide description of the source required in the form "Give me <resource description>.
+When you think there are no more resources needed, just return an empty list.
+Collect as low amount of resources as possible. Do not collect any information, that does not
+directly contribute to solving the task.
+
+Otherwise, for each item, provide description of the source required in the form "Give me <resource description>.
 I expect <form of the result/response>. For example:
 [
     "I need to find the definition of the term 'machine learning'. I expect to get it as a plain text.",
     "I need a Pythonic program for calculating the least common divisor. I expect to get it as a Python code snippet."
     ...
 ]
-Do not write anything else.
+Do not write anything else except the list.
 """
 
 
 _RESOURCE_FORM_PROMPT = """
-You are a helpful assistant that helps me to find the resource in the required form.
+You are a helpful assistant that is focused on deciding the form of a resource.
 
 I will provide you with the following information:
 Query: This is the query that I need to find the resource for.
@@ -101,36 +109,37 @@ class ResourceManager:
     def db(self) -> _ResourceDB:
         return self._resource_db
 
-    def get_resources(self, solution: Solution, use_external: bool = True) -> Solution:
+    def get_new_requests_for_resources(self, solution: Solution) -> list[str]:
         task, context = solution.task, solution.context
+        requests = solution.resources.keys()
         query = (
             f"Task: {task}\n"
             f"Context: {context}\n"
-            f"Solution draft: {solution.solution_structure}\n"
-            f"Existing resources: {str(solution.resources)}"
+            f"Solution structure: {solution.solution_structure}\n"
+            f"Already requested resources: {requests})"
         )
         messages = [SystemMessage(_IDENTIFY_SOURCES_PROMPT), HumanMessage(query)]
-        new_resource_requests = json.loads(str(self._model.invoke(messages).content))
-        new_resources = dict.fromkeys(new_resource_requests, EMPTY_RESOURCE)
+        new_requests = json.loads(str(self._model.invoke(messages).content))
+        return new_requests
 
+    def get_resources(self, solution: Solution, use_external: bool = True) -> Solution:
+        new_resource_requests = self.get_new_requests_for_resources(solution)
+        new_resources = dict.fromkeys(new_resource_requests, EMPTY_RESOURCE)
         all_resources = solution.resources.copy()
         all_resources.update(new_resources)
 
-        resources_to_collect_or_recall = {
+        resources_to_get = {
             request: value
             for request, value in all_resources.items()
             if value == EMPTY_RESOURCE or not value
         }
+        self._recall_resources_from_memory(solution.task, solution.context, resources_to_get)
+        self._get_new_resources(solution.task, solution.context, resources_to_get, use_external)
 
-        self._recall_resources_from_memory(task, context, resources_to_collect_or_recall)
-        self._get_new_requested_resources(
-            task, context, resources_to_collect_or_recall, use_external
-        )
-
-        solution.resources.update(resources_to_collect_or_recall)
+        solution.resources.update(resources_to_get)
         return solution
 
-    def _get_new_requested_resources(
+    def _get_new_resources(
         self,
         task: str,
         context: str,
@@ -148,7 +157,6 @@ class ResourceManager:
             else:
                 result = self._model.invoke(messages).content
             requested_resources[request] = str(result)
-
             form = self._get_resource_form(request)
             self._resource_db.add(
                 _Resource(form=form, context=context, request=request, content=result)
